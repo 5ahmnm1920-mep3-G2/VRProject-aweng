e6cd484d329de20aca068c6ffc9151bcb3218abf
aweng <annawenger@gmx.at>
2020-02-18 21:14:05 +0100

    added sign

---
+++ Assets/MyGame/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    float rotSpeed = 20;
+
+    void OnMouseDrag()
+    {
+        float rotY = Input.GetAxis ("Mouse Y")*rotSpeed*Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.right, rotY);
+    }
+
+}
